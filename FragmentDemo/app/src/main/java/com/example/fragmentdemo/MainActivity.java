68ba07d96a894e98aafb739a5efb16dadbac31d8
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2022-04-08 16:15:22 +0200

    Re-push the lost files. Bc I made a rollback accidentally

--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.fragmentdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    //Ez a menü megjelenítésére szolgál.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    //Ez a függvény meg a menü sikeres kijelölésére.
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                showMessage("Főoldal");
+                break;
+            case R.id.action_profile:
+                showMessage("Profil");
+                break;
+            case R.id.action_settings:
+                showMessage("Beállítások");
+                break;
+        }
+        return true;
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
