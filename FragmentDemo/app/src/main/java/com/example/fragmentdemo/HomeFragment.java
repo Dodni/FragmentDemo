18052deffe20d637e8938b10b1a1222e7f470292
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2022-04-08 19:20:18 +0200

    final

--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/HomeFragment.java
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
@@ -14,6 +15,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TextView textView2 = view.findViewById(R.id.textView4);
+        MainActivity mainActivity = ((MainActivity)getActivity());
+        String profileName = mainActivity.getProfileName();
+        if (profileName != null && profileName.isEmpty()){
+            textView2.setText(profileName);
+        }
+        return view;
     }
 }
--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.fragmentdemo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String profileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+
+        loadFragment(new HomeFragment(),"home", false);
+    }
+
+    private void loadFragment(Fragment fragment, String tag, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment, tag);
+        if(addToBackStack) {
+            transaction.addToBackStack(tag);
+        }
+        transaction.commit();
     }
 
     //Ez a menü megjelenítésére szolgál.
@@ -32,13 +48,17 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_home:
-                showMessage("Főoldal");
+                //showMessage("Főoldal");
+                loadFragment(new HomeFragment(), "home", true);
                 break;
             case R.id.action_profile:
-                showMessage("Profil");
+                //showMessage("Profil");
+                loadFragment(new ProfilFragment(), "profil", true);
                 break;
             case R.id.action_settings:
-                showMessage("Beállítások");
+                //showMessage("Beállítások");
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
                 break;
         }
         return true;
@@ -46,5 +66,24 @@ public class MainActivity extends AppCompatActivity {
 
     private void showMessage(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToHome() {
+        loadFragment(new HomeFragment(),"home", true);
+    }
+
+    public void saveAndNavigateToHome(String profileName) {
+        this.profileName = this.profileName;
+        navigateToHome();
+    }
+
+    // home fragment hívja
+    public String getProfileName() {
+        return profileName;
+    }
+
+    // profile frament hívja
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
     }
 }
